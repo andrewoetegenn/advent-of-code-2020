61b0cc2e82e5a7cb5e338edda1ac3dbbd5ea9a34
Andrew Oetegenn <andrewoetegenn@gmail.com>
2020-12-06 12:02:20 +0000

    Day 1 solution

---
+++ Day 1/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('./Day 1/report.txt', 'utf8');
+const entries = data.split('\r\n').map(e => parseInt(e));
+
+for (let i = 0; i < entries.length; i++) {
+    const firstEntry = entries[i];
+
+    for (let j = 0; j < entries.length; j++) {
+        const secondEntry = entries[j];
+
+        for (let k = 0; k < entries.length; k++) {
+            const thirdEntry = entries[k];
+
+            if (firstEntry + secondEntry + thirdEntry === 2020 ) {
+                console.log(firstEntry * secondEntry * thirdEntry);
+                return;
+            }
+        }
+    }
+}
